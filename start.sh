1dfa3d0c29e557c7f1aa7f3b634c206a829f2c85
Yelsnik <kingsleyokgeorge@gmail.com>
2024-10-24 14:21:40 +0100

    source

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 set -e 
 
-
 echo "run db migration $DB_SOURCE"
 source /app/app.env
+echo /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up 
 
 echo "start the app"
