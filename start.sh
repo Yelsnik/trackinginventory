293371e9c54bfe90c0490c27ed7fdc92c88b143d
Yelsnik <kingsleyokgeorge@gmail.com>
2024-10-25 08:39:33 +0100

    sourcing

--- start.sh
+++ start.sh
@@ -2,23 +2,8 @@
 
 set -e 
 
-file="/app/app.env"
-
-echo "Checking for $file..."
-
-if [ ! -f "$file" ]; then
-  echo "Creating missing app.env file."
-  touch "$file"
-else
-  echo "Found $file, sourcing it."
-fi
-
-# Load environment variables from app.env
-set -a
-. "$file"
-set +a
-
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up 
 
 echo "start the app"
