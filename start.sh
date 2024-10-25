2cd4f4f7834ca96ff6f298d1840ea096529ae7e1
Yelsnik <kingsleyokgeorge@gmail.com>
2024-10-25 07:53:24 +0100

    added code to start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,17 @@
 
 set -e 
 
-echo "run db migration $DB_SOURCE"
+file="/app/app.env"
+
+echo "run db migration"
+
+if ! [ -f /app/app.env ]; then
+  touch $file
+  echo "file "$file" exists!"
+  source /app/app.env
+fi
+
+
 source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up 
 
